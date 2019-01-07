a2c94d2ac399e8d35316927670fe0b8f92109d19
Benwei Zhu <benweizhu@gmail.com>
2019-01-07 20:20:29 +0800

    encoding is correct

--- src/main/java/com/springurlcomponent/example/example/ExampleController.java
+++ src/main/java/com/springurlcomponent/example/example/ExampleController.java
@@ -2,12 +2,16 @@ package com.springurlcomponent.example.example;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 public class ExampleController {
 
-  @GetMapping(value = "/url")
-  public String urlComponent() {
-    return "url";
+  @GetMapping(value = "/url/{id}")
+  public String urlComponent(String id) {
+    return UriComponentsBuilder.fromUriString("history")
+        .pathSegment(id)
+        .path("history")
+        .queryParam("filter[number]", "10").toUriString();
   }
 }
